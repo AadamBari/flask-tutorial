d1e04a4f649604eadfefc8f403ca452a1acc447a
Aadam Bari <aadambari@msn.com>
2019-10-30 13:37:04 +0100

    setting up application structure

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-# decorators (just like views) catch the url and rediect to function below
-@app.route("/")
-@app.route("/index")
-
-# function called at index url
-def index():
-    return "<h1>Hello World!</h1>"
+from application import app
